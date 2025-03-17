f185a6800c736fb6969bdf1338b779bcc70c29e9
Mohit Prajapat <mohit.prajapat@trootech.com>
2025-03-17 16:35:17 +0530

    [Remove Channel] Layer

--- settings/prod.py
+++ settings/prod.py
@@ -4,11 +4,3 @@ from settings.base import config
 DEBUG = False
 
 ALLOWED_HOSTS = ["*", config.get("ALLOWED_HEADER_IP"), config.get("ALLOWED_HEADER_DNS")]
-CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [("redis", 6379)],
-        },
-    },
-}
